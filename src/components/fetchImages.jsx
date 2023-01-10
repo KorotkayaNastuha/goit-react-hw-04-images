cffd52db28b42768c44801cebf4c788652585fdd
KorotkayaNastuha <skiteikina11@gmail.com>
2023-01-10 17:27:51 +0200

    ddd

--- src/components/fetchImages.jsx
+++ src/components/fetchImages.jsx
@@ -1,20 +1,4 @@
-// import axios from 'axios';
 
-// // const key = 31282203 - 7ea8db37884084c288d3f697d;
-// axios.defaults.baseURL = `https://pixabay.com/api`;
-
-// export const fetchImages = async (value, page) => {
-//   const response = await axios.get(`/?q=${value}&page=${page}&key=31282203 - 7ea8db37884084c288d3f697d&image_type=photo&orientation=horizontal&per_page=12`
-//   );
-//   return response.data.hits.map(image => {
-//     return {
-//       id: image.id,
-//       webformatURL: image.webformatURL,
-//       largeImageURL: image.largeImageURL,
-//       tags: image.tags,
-//     };
-//   });
-// };
 
 export { fetchImages };
 // const key = 31282203 - 7ea8db37884084c288d3f697d;
